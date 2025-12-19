2c88d8e347c91835a1157b7205eb54bef79d378e
Diana Sitnikova <sitnickova.diana@gmail.com>
2025-12-19 20:52:28 +0300

    Add files via upload

---
+++ Unique/Unique.h
@@ -0,0 +1,18 @@
+#ifndef UNIQUE_H
+#define UNIQUE_H
+
+template <class T>
+T* Unique(T* first, T* last) {
+    if (first == last) { return last; }
+
+    T* write = first + 1;
+    for (T* read = first + 1; read != last; ++read) {
+        if (*(write - 1) < *read) {
+            *write = *read;
+            ++write;
+        }
+    }
+    return write;
+}
+
+#endif
