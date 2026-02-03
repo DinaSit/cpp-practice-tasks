24769aea1ff170d7309a38a0f0386d1cb3159071
Diana Sitnikova <sitnickova.diana@gmail.com>
2026-02-03 12:05:50 +0300

    Update Remove.h

--- template-functions/Remove/Remove.h
+++ template-functions/Remove/Remove.h
@@ -3,6 +3,9 @@
 
 template <class T>
 T* Remove(T* src_begin, T* src_end, const T& value) {
+    if (!src_begin || !src_end) return src_end;
+    if (src_begin >= src_end) return src_end;
+
     T* write = src_begin;
     while (src_begin < src_end) {
         if (*src_begin != value) { *write = *src_begin; ++write; }
